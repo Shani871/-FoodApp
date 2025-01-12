433ec804a57f51753554b0d2b69476aa3b78343e
Shani871 <chauhan145@gmail.com>
2025-01-12 09:28:01 +0530

    Profile Setting

--- DjangoProject1/urls.py
+++ DjangoProject1/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+import users
 from users.views import register
 from django.contrib.auth import views as auth_views
 
@@ -12,6 +13,7 @@ urlpatterns = [
     path('register/',register,name='register'),  # Your app's URL inclusion
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('profile/', users.views.profile, name='profile'),
   ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
@@ -19,5 +20,6 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
-
-
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
